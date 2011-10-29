fc5e770f724a4fef6edf66a1d0a1229e1949c04f
Paul Bourke <pauldbourke@gmail.com>
2011-10-29 16:08:37 +0100

    Abstracted Img class from demo to a library class
        - Added two new classes, MultiTouchEntity, which is a generic
          abstraction of the Img class from the demo, and ImageEntity, a
          subclass of MultiTouchEntity
    
        - Modified demo to use new classes
    
        - Updated project files to build with sdk r14

--- demo/MTPhotoSortr/src/org/metalev/multitouch/photosortr/PhotoSortrView.java
+++ demo/MTPhotoSortr/src/org/metalev/multitouch/photosortr/PhotoSortrView.java
@@ -15,6 +15,8 @@ import org.metalev.multitouch.controller.MultiTouchController;
 import org.metalev.multitouch.controller.MultiTouchController.MultiTouchObjectCanvas;
 import org.metalev.multitouch.controller.MultiTouchController.PointInfo;
 import org.metalev.multitouch.controller.MultiTouchController.PositionAndScale;
+import org.metalev.multitouch.controller.MultiTouchEntity;
+import org.metalev.multitouch.controller.ImageEntity;
 
 import android.content.Context;
 import android.content.res.Configuration;
@@ -29,15 +31,16 @@ import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 
-public class PhotoSortrView extends View implements MultiTouchObjectCanvas<PhotoSortrView.Img> {
+public class PhotoSortrView extends View implements MultiTouchObjectCanvas<MultiTouchEntity> {
 
 	private static final int[] IMAGES = { R.drawable.m74hubble, R.drawable.catarina, R.drawable.tahiti, R.drawable.sunset, R.drawable.lake };
 
-	private ArrayList<Img> mImages = new ArrayList<Img>();
+	private ArrayList<MultiTouchEntity> mImages = new ArrayList<MultiTouchEntity>();
 
 	// --
 
-	private MultiTouchController<Img> multiTouchController = new MultiTouchController<Img>(this);
+	private MultiTouchController<MultiTouchEntity> multiTouchController = 
+        new MultiTouchController<MultiTouchEntity>(this);
 
 	// --
 
@@ -52,6 +55,10 @@ public class PhotoSortrView extends View implements MultiTouchObjectCanvas<Photo
 	// --
 
 	private Paint mLinePaintTouchPointCircle = new Paint();
+
+    private static final float SCREEN_MARGIN = 100;
+
+    private int width, height, displayWidth, displayHeight;
 
 	// ---------------------------------------------------------------------------------------------------
 
@@ -71,21 +78,33 @@ public class PhotoSortrView extends View implements MultiTouchObjectCanvas<Photo
 	private void init(Context context) {
 		Resources res = context.getResources();
 		for (int i = 0; i < IMAGES.length; i++)
-			mImages.add(new Img(IMAGES[i], res));
+			mImages.add(new ImageEntity(IMAGES[i], res));
 
 		mLinePaintTouchPointCircle.setColor(Color.YELLOW);
 		mLinePaintTouchPointCircle.setStrokeWidth(5);
 		mLinePaintTouchPointCircle.setStyle(Style.STROKE);
 		mLinePaintTouchPointCircle.setAntiAlias(true);
 		setBackgroundColor(Color.BLACK);
+
+        DisplayMetrics metrics = res.getDisplayMetrics();
+        this.displayWidth = res.getConfiguration().orientation == 
+            Configuration.ORIENTATION_LANDSCAPE ? Math.max(metrics.widthPixels,
+            metrics.heightPixels) : Math.min(metrics.widthPixels, metrics.heightPixels);
+        this.displayHeight = res.getConfiguration().orientation == 
+            Configuration.ORIENTATION_LANDSCAPE ? Math.min(metrics.widthPixels,
+            metrics.heightPixels) : Math.max(metrics.widthPixels, metrics.heightPixels);
 	}
 
 	/** Called by activity's onResume() method to load the images */
 	public void loadImages(Context context) {
-		Resources res = context.getResources();
 		int n = mImages.size();
-		for (int i = 0; i < n; i++)
-			mImages.get(i).load(res);
+		for (int i = 0; i < n; i++) {
+            float cx = SCREEN_MARGIN + (float) 
+                (Math.random() * (displayWidth - 2 * SCREEN_MARGIN));
+            float cy = SCREEN_MARGIN + (float) 
+                (Math.random() * (displayHeight - 2 * SCREEN_MARGIN));
+			mImages.get(i).load(context, cx, cy);
+        }
 	}
 
 	/** Called by activity's onPause() method to free memory used for loading the images */
@@ -136,11 +155,11 @@ public class PhotoSortrView extends View implements MultiTouchObjectCanvas<Photo
 	}
 
 	/** Get the image that is under the single-touch point, or return null (canceling the drag op) if none */
-	public Img getDraggableObjectAtPoint(PointInfo pt) {
+	public MultiTouchEntity getDraggableObjectAtPoint(PointInfo pt) {
 		float x = pt.getX(), y = pt.getY();
 		int n = mImages.size();
 		for (int i = n - 1; i >= 0; i--) {
-			Img im = mImages.get(i);
+			ImageEntity im = (ImageEntity) mImages.get(i);
 			if (im.containsPoint(x, y))
 				return im;
 		}
@@ -151,7 +170,7 @@ public class PhotoSortrView extends View implements MultiTouchObjectCanvas<Photo
 	 * Select an object for dragging. Called whenever an object is found to be under the point (non-null is returned by getDraggableObjectAtPoint())
 	 * and a drag operation is starting. Called with null when drag op ends.
 	 */
-	public void selectObject(Img img, PointInfo touchPoint) {
+	public void selectObject(MultiTouchEntity img, PointInfo touchPoint) {
 		currTouchPoint.set(touchPoint);
 		if (img != null) {
 			// Move image to the top of the stack when selected
@@ -164,7 +183,7 @@ public class PhotoSortrView extends View implements MultiTouchObjectCanvas<Photo
 	}
 
 	/** Get the current position and scale of the selected image. Called whenever a drag starts or is reset. */
-	public void getPositionAndScale(Img img, PositionAndScale objPosAndScaleOut) {
+	public void getPositionAndScale(MultiTouchEntity img, PositionAndScale objPosAndScaleOut) {
 		// FIXME affine-izem (and fix the fact that the anisotropic_scale part requires averaging the two scale factors)
 		objPosAndScaleOut.set(img.getCenterX(), img.getCenterY(), (mUIMode & UI_MODE_ANISOTROPIC_SCALE) == 0,
 				(img.getScaleX() + img.getScaleY()) / 2, (mUIMode & UI_MODE_ANISOTROPIC_SCALE) != 0, img.getScaleX(), img.getScaleY(),
@@ -172,182 +191,16 @@ public class PhotoSortrView extends View implements MultiTouchObjectCanvas<Photo
 	}
 
 	/** Set the position and scale of the dragged/stretched image. */
-	public boolean setPositionAndScale(Img img, PositionAndScale newImgPosAndScale, PointInfo touchPoint) {
+	public boolean setPositionAndScale(MultiTouchEntity img, 
+            PositionAndScale newImgPosAndScale, PointInfo touchPoint) {
 		currTouchPoint.set(touchPoint);
-		boolean ok = img.setPos(newImgPosAndScale);
+		boolean ok = ((ImageEntity)img).setPos(newImgPosAndScale);
 		if (ok)
 			invalidate();
 		return ok;
 	}
 
-	// ----------------------------------------------------------------------------------------------
-
-	class Img {
-		private int resId;
-
-		private Drawable drawable;
-
-		private boolean firstLoad;
-
-		private int width, height, displayWidth, displayHeight;
-
-		private float centerX, centerY, scaleX, scaleY, angle;
-
-		private float minX, maxX, minY, maxY;
-
-		private static final float SCREEN_MARGIN = 100;
-
-		public Img(int resId, Resources res) {
-			this.resId = resId;
-			this.firstLoad = true;
-			getMetrics(res);
-		}
-
-		private void getMetrics(Resources res) {
-			DisplayMetrics metrics = res.getDisplayMetrics();
-			// The DisplayMetrics don't seem to always be updated on screen rotate, so we hard code a portrait
-			// screen orientation for the non-rotated screen here...
-			// this.displayWidth = metrics.widthPixels;
-			// this.displayHeight = metrics.heightPixels;
-			this.displayWidth = res.getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE ? Math.max(metrics.widthPixels,
-					metrics.heightPixels) : Math.min(metrics.widthPixels, metrics.heightPixels);
-			this.displayHeight = res.getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE ? Math.min(metrics.widthPixels,
-					metrics.heightPixels) : Math.max(metrics.widthPixels, metrics.heightPixels);
-		}
-
-		/** Called by activity's onResume() method to load the images */
-		public void load(Resources res) {
-			getMetrics(res);
-			this.drawable = res.getDrawable(resId);
-			this.width = drawable.getIntrinsicWidth();
-			this.height = drawable.getIntrinsicHeight();
-			float cx, cy, sx, sy;
-			if (firstLoad) {
-				cx = SCREEN_MARGIN + (float) (Math.random() * (displayWidth - 2 * SCREEN_MARGIN));
-				cy = SCREEN_MARGIN + (float) (Math.random() * (displayHeight - 2 * SCREEN_MARGIN));
-				float sc = (float) (Math.max(displayWidth, displayHeight) / (float) Math.max(width, height) * Math.random() * 0.3 + 0.2);
-				sx = sy = sc;
-				firstLoad = false;
-			} else {
-				// Reuse position and scale information if it is available
-				// FIXME this doesn't actually work because the whole activity is torn down and re-created on rotate
-				cx = this.centerX;
-				cy = this.centerY;
-				sx = this.scaleX;
-				sy = this.scaleY;
-				// Make sure the image is not off the screen after a screen rotation
-				if (this.maxX < SCREEN_MARGIN)
-					cx = SCREEN_MARGIN;
-				else if (this.minX > displayWidth - SCREEN_MARGIN)
-					cx = displayWidth - SCREEN_MARGIN;
-				if (this.maxY > SCREEN_MARGIN)
-					cy = SCREEN_MARGIN;
-				else if (this.minY > displayHeight - SCREEN_MARGIN)
-					cy = displayHeight - SCREEN_MARGIN;
-			}
-			setPos(cx, cy, sx, sy, 0.0f);
-		}
-
-		/** Called by activity's onPause() method to free memory used for loading the images */
-		public void unload() {
-			this.drawable = null;
-		}
-
-		/** Set the position and scale of an image in screen coordinates */
-		public boolean setPos(PositionAndScale newImgPosAndScale) {
-			return setPos(newImgPosAndScale.getXOff(), newImgPosAndScale.getYOff(), (mUIMode & UI_MODE_ANISOTROPIC_SCALE) != 0 ? newImgPosAndScale
-					.getScaleX() : newImgPosAndScale.getScale(), (mUIMode & UI_MODE_ANISOTROPIC_SCALE) != 0 ? newImgPosAndScale.getScaleY()
-					: newImgPosAndScale.getScale(), newImgPosAndScale.getAngle());
-			// FIXME: anisotropic scaling jumps when axis-snapping
-			// FIXME: affine-ize
-			// return setPos(newImgPosAndScale.getXOff(), newImgPosAndScale.getYOff(), newImgPosAndScale.getScaleAnisotropicX(),
-			// newImgPosAndScale.getScaleAnisotropicY(), 0.0f);
-		}
-
-		/** Set the position and scale of an image in screen coordinates */
-		private boolean setPos(float centerX, float centerY, float scaleX, float scaleY, float angle) {
-			float ws = (width / 2) * scaleX, hs = (height / 2) * scaleY;
-			float newMinX = centerX - ws, newMinY = centerY - hs, newMaxX = centerX + ws, newMaxY = centerY + hs;
-			if (newMinX > displayWidth - SCREEN_MARGIN || newMaxX < SCREEN_MARGIN || newMinY > displayHeight - SCREEN_MARGIN
-					|| newMaxY < SCREEN_MARGIN)
-				return false;
-			this.centerX = centerX;
-			this.centerY = centerY;
-			this.scaleX = scaleX;
-			this.scaleY = scaleY;
-			this.angle = angle;
-			this.minX = newMinX;
-			this.minY = newMinY;
-			this.maxX = newMaxX;
-			this.maxY = newMaxY;
-			return true;
-		}
-
-		/** Return whether or not the given screen coords are inside this image */
-		public boolean containsPoint(float scrnX, float scrnY) {
-			// FIXME: need to correctly account for image rotation
-			return (scrnX >= minX && scrnX <= maxX && scrnY >= minY && scrnY <= maxY);
-		}
-
-		public void draw(Canvas canvas) {
-			canvas.save();
-			float dx = (maxX + minX) / 2;
-			float dy = (maxY + minY) / 2;
-			drawable.setBounds((int) minX, (int) minY, (int) maxX, (int) maxY);
-			canvas.translate(dx, dy);
-			canvas.rotate(angle * 180.0f / (float) Math.PI);
-			canvas.translate(-dx, -dy);
-			drawable.draw(canvas);
-			canvas.restore();
-		}
-
-		public Drawable getDrawable() {
-			return drawable;
-		}
-
-		public int getWidth() {
-			return width;
-		}
-
-		public int getHeight() {
-			return height;
-		}
-
-		public float getCenterX() {
-			return centerX;
-		}
-
-		public float getCenterY() {
-			return centerY;
-		}
-
-		public float getScaleX() {
-			return scaleX;
-		}
-
-		public float getScaleY() {
-			return scaleY;
-		}
-
-		public float getAngle() {
-			return angle;
-		}
-
-		// FIXME: these need to be updated for rotation
-		public float getMinX() {
-			return minX;
-		}
-
-		public float getMaxX() {
-			return maxX;
-		}
-
-		public float getMinY() {
-			return minY;
-		}
-
-		public float getMaxY() {
-			return maxY;
-		}
-	}
+    public boolean pointInObjectGrabArea(PointInfo pt, MultiTouchEntity img) {
+        return false;
+    }
 }
